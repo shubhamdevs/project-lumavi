b3dc526870fa7375bd4cfcd7e9ef63c53cb24801
suvm.dev <suvm.dev@gmail.com>
2026-06-27 11:16:46 +0530

    Clean slate: remove all files to restart project fresh (retaining only .gitignore)

--- frontend/src/app/(auth)/register/[[...rest]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from '@clerk/nextjs';
-
-export default function RegisterPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <SignUp routing="hash" />
-    </div>
-  );
-}
--- frontend/src/lib/generation/brand-extractor.ts
+++
@@ -1,9 +0,0 @@
-export type BrandSignals = {
-  colorMood: string;
-  styleDescriptors: string[];
-  formality: string;
-  brandPersonality: string;
-  photographyStyle: string;
-  brandIsNot: string;
-  typographyFeel: string;
-};
